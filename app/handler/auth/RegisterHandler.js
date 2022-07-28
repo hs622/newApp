692be83d54db9a601bb63a9e02ee820c5b1e34ac
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2022-07-28 21:04:11 +0500

    bug fixed

--- app/handler/auth/RegisterHandler.js
+++ app/handler/auth/RegisterHandler.js
@@ -1,40 +1,36 @@
-const { exist } = require('joi');
-const validateRegisterRequest = require('../../request/registerRequest');
-const Constant = require('../../../constant');
+const { exist } = require("joi");
+const validateRegisterRequest = require("../../request/registerRequest");
+const Constant = require("../../../constant");
 
 module.exports = async (request, response) => {
+  try {
+    const errors = await validateRegisterRequest(request.body);
 
-	await validateRegisterRequest(request.body)
-		.then((body) => {
-			console.log("validation: ", body)
-		})
-		.catch((errors) => {
-			let res = errors.details.map(error => error.message);
-			return response.status(Constant.Status.UnprocessableEntity)
-				.send(res)
-				.end();
-		})
-		
-		
-	// if (!errors.isEmpty())
-	// 	return response.status(422).json({ 
-	// 		success: false, 
-	// 		errors: errors.array() 
-	// 	});
-	// else
-	// result.newValue = {
-	// 	"text" : "value"
-	// }
-		return response.send(
+    return response.status(200).send(errors);
+  } catch (errors) {
+    let res = errors.details.map((error) => error.message);
+    return response.status(Constant.Status.UnprocessableEntity).send(res);
+  }
 
-			"wait"
-			// JSON.stringify(Validate.result.details)
-				// JSON.stringify({
-				// 	"path": request.url, 
-				// 	"method": request.method, 
-				// 	"headers": request.rawHeaders,
-				// 	"body": request.body
-				// })
-				)
-				.end();
-}
+  // if (!errors.isEmpty())
+  // 	return response.status(422).json({
+  // 		success: false,
+  // 		errors: errors.array()
+  // 	});
+  // else
+  // result.newValue = {
+  // 	"text" : "value"
+  // }
+  // return response.send(
+
+  // 	"wait"
+  // 	// JSON.stringify(Validate.result.details)
+  // 		// JSON.stringify({
+  // 		// 	"path": request.url,
+  // 		// 	"method": request.method,
+  // 		// 	"headers": request.rawHeaders,
+  // 		// 	"body": request.body
+  // 		// })
+  // 		)
+  // 		.end();
+};
